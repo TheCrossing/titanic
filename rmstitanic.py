810e3b8773b4d595c2ace95a92fb80fb2564e956
Benjamin Henehan <benjaminhenehan@gmail.com>
2018-11-25 10:54:59 +1100

    added cleandata function for training data

--- rmstitanic.py
+++ rmstitanic.py
@@ -4,10 +4,13 @@ import pandas as pd
 train = pd.read_csv('train.csv')
 test = pd.read_csv('test.csv')
 
-train = train.drop(['Cabin'], axis = 1)#because there is so much missing data from cabin just remove it
-train['Embarked'] = train['Embarked'].fillna('S') #so can just replace with the most common value which is S
-for pclass in train['Pclass'].unique(): #for every unique pclass, locate the age column value, match to groupe by Pclass means
-    train.loc[train['Pclass'] == pclass, 'Age'] = train.groupby('Pclass')['Age'].mean().loc[pclass]
+def cleandata(df):
+	df = train.drop(['Cabin'], axis = 1)#because there is so much missing data from cabin just remove it
+	df['Embarked'] = train['Embarked'].fillna('S') #so can just replace with the most common value which is S
+	for pclass in df['Pclass'].unique(): #for every unique pclass, locate the age column value, match to groupe by Pclass means
+	    df.loc[df['Pclass'] == pclass, 'Age'] = df.groupby('Pclass')['Age'].mean().loc[pclass]
+	return df
 
 
+train = cleandata(train)
 print (train.head())
